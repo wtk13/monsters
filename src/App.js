0ff92e0157f51ca5e3827e81aeb9c39428afffb9
rw <remigiusz.wasiak@gmail.com>
2019-01-13 18:33:03 +0100

    succeed api connection and data fetched

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import './styles/main.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getAllMonsters();
+  }
+
+  getAllMonsters() {
+    fetch('http://localhost:8080/api/v1/monsters')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Can not connect to the API');
+      })
+      .then(response => {
+        this.setState({monsters: response.data});
+      }).catch(err => Promise.reject(err.message));
+  }
+
   render() {
+    const { monsters } = this.state;
+
     return (
       <div className='appWrapper'>
-        <p>W tym miejscu znajdzie się zawartość widgeta</p>
+        <h1>Monsters list</h1>
+        <ul>
+          { monsters.map((monster, index) => (
+            <li key={index}>{monster.name}</li>
+          )) }
+        </ul>
       </div>
     );
   }
