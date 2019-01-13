87ec86e14285dee17a6c4cb54be457957e517014
rw <remigiusz.wasiak@gmail.com>
2019-01-14 00:43:22 +0100

    main view structure changed and basic styles added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MonstersList from './view/MonstersList';
 import MonsterDetails from './view/MonsterDetails';
+import About from './view/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import './styles/main.scss';
+
 
 class App extends Component {
 
   render() {
     return (
       <Router>
-        <div>
-          <Route path="/" exact={true} render={ () => <MonstersList /> } />
-          <Route path='/:slug' component={ MonsterDetails } />
+        <div className='appWrapper'>
+          <div className='appWrapper__view'>
+            <Header />
+            <main className='mainContent'>
+              <Route path="/" exact={true} render={ () => <MonstersList /> } />
+              <Route path='/monster/:slug' component={ MonsterDetails } />
+              <Route path='/about' component={ About }/>
+            </main>
+            <Footer />
+          </div>
         </div>
       </Router>
     );
