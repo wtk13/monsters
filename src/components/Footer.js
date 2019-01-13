856ec94d5fb4a355a669103392487518dbe24d90
rw <remigiusz.wasiak@gmail.com>
2019-01-14 00:41:31 +0100

    Footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className='mainFooter'>
+        <p>Author: Remigiusz Wasiak</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
