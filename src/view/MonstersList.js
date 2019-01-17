da115f4511f794bc02ce5c1df6d79962ca5194fc
rw <remigiusz.wasiak@gmail.com>
2019-01-17 20:11:32 +0100

    monsters list view draft created

--- src/view/MonstersList.js
+++ src/view/MonstersList.js
@@ -52,30 +52,30 @@ class MonstersList extends Component {
         <article className='widgetContent'>
           <h1 className='widgetContent__header'>Choose your monster</h1>
           { monsters ? (
-            <div id='carouselExampleIndicators' className='carousel slide' data-ride='carousel'>
+            <div className='carousel slide' data-ride='carousel'>
             <ol className='carousel-indicators'>
               { monsters.map((monster, index) => (
-                <li key={index}
-                    data-target='#carouselExampleIndicators'
-                    data-slide-to={index}
-                    className={`${index === activeSlide ? 'active' : ''}`}
-                    onClick={this.setSlide}></li>
+                <li key={ index }
+                    data-slide-to={ index }
+                    className={ `${index === activeSlide ? 'active' : ''}` }
+                    onClick={ this.setSlide }></li>
               )) }
             </ol>
               <div className='carousel-inner'>
                 { monsters.map((monster, index) => (
-                  <div key={index} className={`carousel-item ${index === activeSlide ? 'active' : ''}`}>
-                    <div style={{height: 340, display: 'flex', justifyContent: 'center', padding: '0px 50px 50px'}}>
-                      <img style={{maxWidth: '100%', maxHeight: '100%'}}src={monster.images.big} alt={monster.name}/>
+                  <div key={ index } className={ `carousel-item ${index === activeSlide ? 'active' : ''} slideWrapper` }>
+                    <div style={{ height: 340, display: 'flex', justifyContent: 'center', padding: '0px 50px 50px', flexDirection: 'column' }}>
+                      <img style={{ maxWidth: '100%', maxHeight: '100%'}}src={ monster.images.big } alt={ monster.name }/>
+                      <p style={{ textAlign: 'center', marginBottom: 0 }}>{ monster.name }</p>
                     </div>
                   </div>
                 ))}
               </div>
-              <a className='carousel-control-prev' href='' role='button' data-slide='prev' onClick={this.prevSlide}>
+              <a className='carousel-control-prev' href='' role='button' data-slide='prev' onClick={ this.prevSlide }>
                 <span className='carousel-control-prev-icon' aria-hidden='true'></span>
                 <span className='sr-only'>Previous</span>
               </a>
-              <a className='carousel-control-next' href='' role='button' data-slide='next' onClick={this.nextSlide}>
+              <a className='carousel-control-next' href='' role='button' data-slide='next' onClick={ this.nextSlide }>
                 <span className='carousel-control-next-icon' aria-hidden='true'></span>
                 <span className='sr-only'>Next</span>
               </a>
