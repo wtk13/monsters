b261454d096a01b34b15ff23a56d73cbb72aa961
rw <remigiusz.wasiak@gmail.com>
2019-01-17 20:12:12 +0100

    Footer minor style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className='mainFooter'>
-        <p>Author: Remigiusz Wasiak</p>
+        <p className='mainFooter__text'>Author: Remigiusz Wasiak</p>
       </footer>
     );
   }
