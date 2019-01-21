36556ac8785fa0340540b53264d9db10376fef07
rw <remigiusz.wasiak@gmail.com>
2019-01-21 20:17:15 +0100

    About view content modified, unnecessary padding removed

--- src/view/About.js
+++ src/view/About.js
@@ -6,15 +6,16 @@ class About extends Component {
       <div className='widgetContent'>
         <h1 className='widgetContent__header'>About page</h1>
         <article className='aboutPageContent'>
-          <p>This is react training app. Below there are listed libraries / technologies used in this project:</p>
+          <p>This is react training app. Below there are listed libraries, technologies and techniques used in this project:</p>
           <ol className='aboutPageContent__list'>
             <li>React v16.7.0</li>
             <li>React Router v4</li>
-            <li>SASS</li>
             <li>Bootstrap v3</li>
             <li>React Bootstrap</li>
             <li>React Spinners</li>
             <li>Monsters local API</li>
+            <li>SASS</li>
+            <li>BEM</li>
           </ol>
         </article>
       </div>
