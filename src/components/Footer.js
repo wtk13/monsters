cb4621c883765de8d0d66de07ee8e3dbd026392c
rw <remigiusz.wasiak@gmail.com>
2019-01-21 21:04:39 +0100

    styled link added in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className='mainFooter'>
-        <p className='mainFooter__text'>&copy; Remigiusz Wasiak</p>
+        <p className='mainFooter__text'><a href="https://www.remigiuszwasiak.pl/" className='navButton'>&copy; Remigiusz Wasiak</a></p>
       </footer>
     );
   }
