82f1629ed7f9189d837d8b0a426f4f8b9d29cac6
Wojtek <rybinski05@gmail.com>
2019-02-03 20:12:14 +0100

    Added body parser

--- monsters-api/index.js
+++ monsters-api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const config = require('./config');
 const Monsters = require('./monsters');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const monsters = new Monsters();
 
 app.use(config.staticDir, express.static('static'));
+app.use(bodyParser.json());
 
 app.use(config.apiPath, (request, response, next) => {
     response.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,30 +17,30 @@ app.use(config.apiPath, (request, response, next) => {
 });
 
 app.get(config.apiPath, (request, response) => {
-    response.status(200).send(JSON.stringify({
+    response.status(200).send({
         data: {
             message: "Hello, Monsters API! 👋"
         }
-    }));
+    });
 });
 
 app.get(`${config.apiPath}/monsters`, (request, response) => {
-    response.status(200).send(JSON.stringify({
+    response.status(200).send({
         data: monsters.getMonstersList()
-    }));
+    });
 });
 
 app.get(`${config.apiPath}/monster/:name`, (request, response) => {
     const monster = monsters.getMonster(request.params.name);
 
     if(monster) {
-        response.status(200).send(JSON.stringify({
+        response.status(200).send({
             data: monster
-        }));
+        });
         return;
     }
 
-    response.status(404).send(JSON.stringify({error: `"${request.params.name}" monster not found 🙁`}));
+    response.status(404).send({error: `"${request.params.name}" monster not found 🙁`});
 });
 
 app.get(`${config.apiPath}/teapot`, (request, response) => {
